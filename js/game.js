82b8090f22fb9e0946063b9d1b7d14890ff977b2
Rob Prouse <rob@prouse.org>
2018-03-23 15:25:00 -0400

    Maze generation is now working

--- js/game.js
+++ js/game.js
@@ -5,8 +5,12 @@ let xBlockSize;
 let yBlockSize;
 
 let maze;
+let maze_visited; // have cells been visited or not
+let stack;
+let current;
+let maze_complete;
 
-const MAZE_SIZE = 10;
+const MAZE_SIZE = 4;
 const FRAMES_PER_SECOND = 4;
 
 // Cell sides. If the flag is on, it indicates that direction is open.
@@ -24,26 +28,44 @@ window.onload = function() {
 }
 
 function init() {
-  // Initialize the Maze
-  maze = [];
-  for(var y = 0; y < MAZE_SIZE; y++) {
-    row = [];
-    for(var x = 0; x < MAZE_SIZE; x++) {
-      row.push(0);
-    }
-    maze.push(row);
-  }
-  maze[0][0] = RIGHT;
-  maze[1][0] = LEFT;
-
-  maze[9][0] = BOTTOM;
-  maze[9][1] = TOP;
   xBlockSize = canvas.width / MAZE_SIZE;
   yBlockSize = canvas.height / MAZE_SIZE;
+
+  // Initialize the Maze
+  maze = [];
+  maze_visited = [];
+  for(var y = 0; y < MAZE_SIZE; y++) {
+    row = [];
+    visited_row = [];
+    for(var x = 0; x < MAZE_SIZE; x++) {
+      row.push(0);
+      visited_row.push(false);
+    }
+    maze.push(row);
+    maze_visited.push(visited_row);
+  }
+
+  current = { x: 0, y: 0 };
+  stack = [];
+
+  maze_complete = false;
+  visited(0, 0);
 }
 
 function gameLoop() {
   draw();
+  if(!mazeComplete())
+  {
+    if(hasUnvisitedNeighboors(current.x, current.y)) {
+      let next = randomNeighbor(current.x, current.y);
+      stack.push(current);
+      removeWall(current, next);
+      current = next;
+      visited(current.x, current.y);
+    } else if(stack.length > 0) {
+      current = stack.pop();
+    }
+  }
 }
 
 function draw() {
@@ -68,6 +90,9 @@ function drawCell(x, y) {
   var x1 = xBlockSize + x0;
   var y0 = yBlockSize * y;
   var y1 = yBlockSize + y0;
+  if(current.x === x && current.y === y) {
+    colorRect(x0 + 1, y0 + 1, xBlockSize - 2, yBlockSize - 2, 'green');
+  }
   var cell = maze[x][y];
   if((cell & LEFT) === 0) {
     drawLine(x0, y1, x0, y0);
@@ -101,4 +126,79 @@ function getRandomIntInclusive(min, max) {
 function colorRect(x, y, width, height, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
+}
+
+function visited (x, y) {
+  maze_visited[x][y] = true;
+}
+
+function mazeComplete() {
+  // Cache for efficiency
+  if(maze_complete === true)
+    return true;
+
+  for(var y = 0; y < MAZE_SIZE; y++) {
+    for(var x = 0; x < MAZE_SIZE; x++) {
+      if(maze_visited[x][y] === false)
+        return false;
+    }
+  }
+  maze_complete = true;
+  return true;
+}
+
+function hasUnvisitedNeighboors(x, y) {
+  if(leftUnvisited(x, y)) return true;
+  if(rightUnvisited(x, y)) return true;
+  if(topUnvisited(x, y)) return true;
+  if(bottomUnvisited(x, y)) return true;
+
+  return false;
+}
+
+function randomNeighbor(x, y) {
+  while(true) {
+    let i = getRandomIntInclusive(0,3);
+    if(i === 0 && leftUnvisited(x, y)) {
+      return { x: x - 1, y: y };
+    } else if(i === 1 && topUnvisited(x, y)) {
+      return { x: x, y: y - 1 };
+    } else if(i === 2 && rightUnvisited(x, y)) {
+      return { x: x + 1, y: y };
+    } else if(i === 3 && bottomUnvisited(x, y)) {
+      return { x: x, y: y + 1 };
+    }
+  }
+}
+
+function leftUnvisited(x, y) {
+  return x > 0 && maze_visited[x-1][y] === false;
+}
+
+function rightUnvisited(x, y) {
+  return x < (MAZE_SIZE - 1) && maze_visited[x+1][y] === false;
+}
+
+function topUnvisited(x, y) {
+  return y > 0 && maze_visited[x][y-1] === false;
+}
+
+function bottomUnvisited(x, y) {
+  return y < (MAZE_SIZE - 1) && maze_visited[x][y+1] === false;
+}
+
+function removeWall(one, two) {
+  if(one.x < two.x) { // one is left of two
+    maze[one.x][one.y] |= RIGHT;
+    maze[two.x][two.y] |= LEFT;
+  } else if(one.x > two.x) { // one is right of two
+    maze[one.x][one.y] |= LEFT;
+    maze[two.x][two.y] |= RIGHT;
+  } if(one.y < two.y) { // one is above two
+    maze[one.x][one.y] |= BOTTOM;
+    maze[two.x][two.y] |= TOP;
+  } if(one.y > two.y) { // one is below two
+    maze[one.x][one.y] |= TOP;
+    maze[two.x][two.y] |= BOTTOM;
+  }
 }
