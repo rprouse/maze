923521a983b80dbb8167e31baf7f333b9bfb0301
Rob Prouse <rob@prouse.org>
2018-03-27 16:58:35 -0400

    Added a button to reset the maze

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { Game } from './game';
 
+function init() {
+  let game = new Game();
+  document.getElementById('reset').onclick = () => game.init();
+}
+
 if(document.readyState === "complete") {
   console.log("readyState complete");
-  new Game();
+  init();
 } else {
   window.onload = () => {
     console.log("window.onload");
-    new Game();
+    init();
   };
 }
